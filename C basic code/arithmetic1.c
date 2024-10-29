3e30cff7b94f87eb60b9380d7b92121e34603918
Tandra Pramanik <156818552+Tandra21@users.noreply.github.com>
2024-10-29 20:30:23 +0600

    Add files via upload

---
+++ C basic code/arithmetic1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a=20,b=50;
+    int ans=a-b;
+    printf("%d",ans);
+    return 0;
+}
---
+++ C basic code/arithmetic2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int  main()
+{
+  int a=10,b=50;
+  int dvd=a/b;
+  printf("%d",dvd);
+  return 0;   
+}
