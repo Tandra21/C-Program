ff74269ed4c40fe742050799c87c175a8879da91
Tandra Pramanik <156818552+Tandra21@users.noreply.github.com>
2024-10-31 18:28:30 +0600

    Add files via upload

---
+++ condition_4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int tk;
+  scanf("%d",&tk);
+  if(tk >= 5000)
+  {
+    printf("Cox's Bazar jabo\n");
+  }  
+  else if(tk >= 10000)
+  {
+    printf("Saint Martin jabo\n ");
+  }
+  else
+  {
+    printf("Kothao jabo na\n");
+  }
+  return 0;
+} 
